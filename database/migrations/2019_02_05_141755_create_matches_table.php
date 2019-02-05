e5dec758589e00ff83ff37ceda6f401947ee8fb5
Alex Coenen <coenena21@gmail.com>
2019-02-05 15:52:33 +0100

    setting up dbwedprog (database)

---
+++ database/migrations/2019_02_05_141755_create_matches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->unsignedInteger('group_id');
+            $table->unsignedInteger('awayteam_id');
+            $table->unsignedInteger('homescore');
+            $table->unsignedInteger('awayscore');
+            $table->time('matchstart');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
